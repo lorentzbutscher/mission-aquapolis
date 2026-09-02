3166e4c92d782e125d27889fbb67266008a3f755
Lorentz <lorentzbutscher@gmail.com>
2026-09-02 21:12:18 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // appels Firebase/Firestore ne sont JAMAIS interceptés (on laisse le réseau
 // natif gérer, sync.js gère déjà les échecs proprement).
 
-const CACHE_VERSION = "aquapolis-v23";
+const CACHE_VERSION = "aquapolis-v24";
 
 const APP_SHELL = [
   "./",
