acf0164312a9274b5cb74c67b0bce352214e00f0
Lorentz <lorentzbutscher@gmail.com>
2026-09-10 21:08:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,6 @@ const APP_SHELL = [
   "./manifest.json",
   "./content.json",
   "./css/style.css",
-  "./css/aquapolis-ameliorations.css",
   "./js/app.js",
   "./js/content.js",
   "./js/state.js",
