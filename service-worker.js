a0cc0bdfb493605b3dd51995b9d7a7366b96ce53
Lorentz <lorentzbutscher@gmail.com>
2026-09-11 17:05:36 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // appels Firebase/Firestore ne sont JAMAIS interceptés (on laisse le réseau
 // natif gérer, sync.js gère déjà les échecs proprement).
 
-const CACHE_VERSION = "aquapolis-v32";
+const CACHE_VERSION = "aquapolis-v33";
 
 const APP_SHELL = [
   "./",
